b6a77e35590f8c936e7bd4f2ec364815decaf30f
P-ppc <ppc-user@foxmail.com>
2016-09-30 10:42:40 +0800

    导航:实现点击用户头像/用户名，能够跳转到对应用户的信息页面

--- src/lib/linkHref.js
+++ src/lib/linkHref.js
@@ -2,7 +2,14 @@
 $(function() {
     $(document).on('click', '.topic-link', function() {
         var topicId = $(this).attr("data-id");
+        // 锚点
         var anchor = $(this).attr("data-anchor");
         window.location = 'topicInfo.html?topicId=' + topicId + "&anchor=" + anchor;
+    });
+
+    // 点击用户头像和用户名跳转到用户信息页面
+    $(document).on('click', '.js-userInfoLink', function() {
+        var loginName = $(this).attr("data-loginName");
+        window.location = 'userInfo.html?loginName=' + loginName;
     });
 });
