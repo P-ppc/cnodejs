01a9e6d94370c2dfeb9403e8b4434923f1ef1f38
P-ppc <ppc-user@foxmail.com>
2016-09-30 14:23:25 +0800

    布局:采用css的方式将footer固定在底部, 取代原有的js放方式

--- src/lib/linkHref.js
+++ src/lib/linkHref.js
@@ -1,8 +1,8 @@
 // 负责连接的跳转
 $(function() {
-    window.nodeRequire = require;
-    const electron = nodeRequire('electron');
-    const shell = electron.shell;
+    // window.nodeRequire = require;
+    // const electron = nodeRequire('electron');
+    // const shell = electron.shell;
 
     $(document).on('click', '.topic-link', function() {
         var topicId = $(this).attr("data-id");
@@ -17,19 +17,19 @@ $(function() {
         window.location = 'userInfo.html?loginName=' + loginName;
     });
 
-    // 主题内容和用户回复内容中的超链接使用浏览器打开
-    $(document).on('click', '.markdown-text a', function() {
-        var href = $(this).attr('href');
-        shell.openExternal(href);
-        event.preventDefault();
-    });
+    // // 主题内容和用户回复内容中的超链接使用浏览器打开
+    // $(document).on('click', '.markdown-text a', function() {
+    //     var href = $(this).attr('href');
+    //     shell.openExternal(href);
+    //     event.preventDefault();
+    // });
 
-    // 指定的链接使用浏览器打开
-    $(document).on('click', '.js-externalLink', function() {
-        var href = $(this).attr('href');
-        shell.openExternal(href);
-        event.preventDefault();
-    });
+    // // 指定的链接使用浏览器打开
+    // $(document).on('click', '.js-externalLink', function() {
+    //     var href = $(this).attr('href');
+    //     shell.openExternal(href);
+    //     event.preventDefault();
+    // });
 
     // @用户名连接点击跳转到用户信息
     $(document).on('click', '.markdown-text a[href^=\\/user\\/]', function() {
