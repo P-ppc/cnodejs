67d8774daf6be2fceca6f7426dfce1d251ace432
P-ppc <ppc-user@foxmail.com>
2017-02-08 15:26:51 +0800

    sidebar和tabbar两个controller采用事件广播通信

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,6 @@
 window.APP = angular.module("APP", []);
 
-window.APP.controller("sidebarCtrl", function ($scope) {
+window.APP.controller("sidebarCtrl", function ($rootScope, $scope) {
     $scope.menuItems = [
         {
             title: "首页",
@@ -33,13 +33,69 @@ window.APP.controller("sidebarCtrl", function ($scope) {
             class: "sign-out"
         }
     ];
-    $scope.selectedItem = "HOME";
-    $scope.select = function (value) {
-        $scope.selectedItem = value;
+    $scope.selectedItem = $scope.menuItems[0];
+    $scope.select = function (item) {
+        $scope.selectedItem = item;
+        $rootScope.$broadcast("menuSelect", item);
     };
     // 切换展开或者收缩
     $scope.isExpanded = false;
     $scope.toggleExpand = function () {
         $scope.isExpanded = !$scope.isExpanded;
     };
+    $scope.$on("tabSelect", function (event, tab) {
+        if (tab == null) {
+            $scope.selectedItem = null;
+            return;
+        }
+        var code = tab.menu;
+        for (var i = 0, len = $scope.menuItems.length; i < len; i++) {
+            if (code == $scope.menuItems[i].code) {
+                $scope.selectedItem = $scope.menuItems[i];
+                break;
+            }
+        }
+    });
+});
+
+window.APP.controller("tabCtrl", function ($rootScope, $scope) {
+    $scope.tabItems = [
+        {
+            title: "首页",
+            menu: "HOME"
+        }
+    ];
+    $scope.selectedItem = $scope.tabItems[0];
+    $scope.select = function (item) {
+        $scope.selectedItem = item;
+        $rootScope.$broadcast("tabSelect", item);
+    };
+    $scope.addTab = function () {
+        var newItem = {
+            title: "首页",
+            menu: "HOME"
+        };
+        $scope.tabItems.push(newItem);
+        // 选中tab
+        $scope.select(newItem);
+    };
+    $scope.closeTab = function (item) {
+        var len = $scope.tabItems.length;
+        var index = $scope.tabItems.indexOf(item);
+        // 选中其他项
+        if ($scope.selectedItem == item) {
+            if (len == 1) {
+                $scope.select(null);
+            } else if (index == len - 1) {
+                $scope.select($scope.tabItems[index - 1]);
+            } else {
+                $scope.select($scope.tabItems[index + 1]);
+            }
+        }
+        $scope.tabItems.splice(index, 1);
+    };
+    $scope.$on("menuSelect", function (event, menu) {
+        $scope.selectedItem.title = menu.title;
+        $scope.selectedItem.menu = menu.code;
+    });
 });
