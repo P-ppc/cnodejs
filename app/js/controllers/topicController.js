234863b4d2803610cbf7a219695d46f31931ede9
P-ppc <ppc-user@foxmail.com>
2017-04-22 00:26:30 +0800

    引入mditor作为富文本编辑器

--- app/js/controllers/topicController.js
+++ app/js/controllers/topicController.js
@@ -36,5 +36,11 @@ window.APP.controller('topicsCtrl', ['$scope', '$http', 'TopicListService', func
 }]);
 
 window.APP.controller("topicInfoCtrl", ["$scope", function ($scope) {
-    
+    var mditor =  Mditor.fromTextarea(document.getElementById('editor'));
+
+    //获取或设置编辑器的值
+    mditor.on('ready',function(){
+    console.log(mditor.value);
+    mditor.value = '** hello **';	
+    });
 }]);
