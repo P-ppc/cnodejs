dfbc39147bc292de732cd52868cc4540a949c8aa
P-ppc <ppc-user@foxmail.com>
2017-04-26 16:35:26 +0800

    调整mditor的样式

--- app/js/controllers/topicController.js
+++ app/js/controllers/topicController.js
@@ -37,10 +37,12 @@ window.APP.controller('topicsCtrl', ['$scope', '$http', 'TopicListService', func
 
 window.APP.controller("topicInfoCtrl", ["$scope", function ($scope) {
     var mditor =  Mditor.fromTextarea(document.getElementById('editor'));
-
+    mditor.split = false;	//关闭
+    //是否打开预览			
+    mditor.preivew = false;	//关闭
+    //是否全屏			
+    mditor.fullscreen = false;	//关闭	
     //获取或设置编辑器的值
     mditor.on('ready',function(){
-    console.log(mditor.value);
-    mditor.value = '** hello **';	
     });
 }]);
