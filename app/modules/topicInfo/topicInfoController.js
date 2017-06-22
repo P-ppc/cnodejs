2a0de637dd1084cc656f8f188e547a3aa54fa42a
P-ppc <ppc-user@foxmail.com>
2017-06-22 17:26:37 +0800

    1. 获取主题详情时带上accesstoken参数 2. 实现主题收藏功能

--- app/modules/topicInfo/topicInfoController.js
+++ app/modules/topicInfo/topicInfoController.js
@@ -1,8 +1,9 @@
 window.APP.controller("topicInfoCtrl", [
     "$scope",
     "$stateParams",
+    "MessageService",
     "TopicInfoService",
-    ($scope, $stateParams, TopicInfoService) => {
+    ($scope, $stateParams, MessageService, TopicInfoService) => {
     
     let topicId = $stateParams.topicId,
         mditor = Mditor.fromTextarea(document.getElementById('editor'));
@@ -17,4 +18,25 @@ window.APP.controller("topicInfoCtrl", [
     } 
 
     getTopicInfo();
+
+    $scope.toggleCollect = () => {
+        $scope.busy = true;
+        let promise;
+        if ($scope.topicInfo.is_collect) {
+            promise = TopicInfoService.unCollect($scope.topicInfo.id);
+        } else {
+            promise = TopicInfoService.collect($scope.topicInfo.id);
+        }
+
+        promise.then(resp => {
+            let msg = $scope.topicInfo.is_collect ? "取消收藏成功" : "收藏成功";
+            MessageService.success(msg);
+            $scope.topicInfo.is_collect = !$scope.topicInfo.is_collect;
+        }).catch(error => {
+            let msg = ($scope.topicInfo.is_collect ? "取消收藏失败，" : "收藏失败，") + "请稍后重试";
+            MessageService.error(msg);
+        }).finally(() => {
+            $scope.busy = false;
+        });
+    };
 }]);
--- app/modules/topicInfo/topicInfoService.js
+++ app/modules/topicInfo/topicInfoService.js
@@ -1,13 +1,32 @@
 window.APP.factory('TopicInfoService', [
+    "$rootScope",
     'RequestService',
-    RequestService => {
+    ($rootScope, RequestService) => {
     
     let service = {},
-        infoUrl = 'https://cnodejs.org/api/v1/topic/';
+        infoUrl = 'https://cnodejs.org/api/v1/topic/',
+        collectUrl = 'https://cnodejs.org/api/v1/topic_collect/collect',
+        unCollectUrl = 'https://cnodejs.org/api/v1/topic_collect/de_collect';
 
     service.getInfo = params => {
         let url = infoUrl + params.id;
-        return RequestService.get(url);
+        return RequestService.get(url, {
+            accesstoken: $rootScope.user.accessToken
+        });
+    };
+
+    service.collect = topic_id => {
+        return RequestService.post(collectUrl, {
+            accesstoken: $rootScope.user.accessToken,
+            topic_id: topic_id
+        });
+    };
+
+    service.unCollect = topic_id => {
+        return RequestService.post(unCollectUrl, {
+            accesstoken: $rootScope.user.accessToken,
+            topic_id: topic_id
+        });
     };
 
     return service;
