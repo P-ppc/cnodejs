5b75a7fc0222018c706253cd7f215831db2bdbf5
P-ppc <ppc-user@foxmail.com>
2017-07-03 15:11:47 +0800

    实现mHoverPopup指令用于替代title属性

---
+++ app/modules/directives/hoverPopupDirective.js
@@ -0,0 +1,20 @@
+window.APP.directive("mHoverPopup", [
+    () => {
+    /* 返回元素是否位于Body左侧 */
+    function isAtLeft ($ele) {
+        let bodyWidth = $("body").width();
+        let offset = $ele.offset();
+        return (offset <= bodyWidth / 2);
+    };
+    /* 对于位于Body左侧的指令 添加left类, 右侧的指令 添加right类 */
+    return {
+        restrict: 'A',
+        link: ($scope, $element, $attrs) => {
+            if (isAtLeft($element)) {
+                $element.addClass("left");
+            } else {
+                $element.addClass("right");
+            }
+        }
+    };
+}]);
