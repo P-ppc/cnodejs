b7dd6c55b002c1a4e16f3c97c61ae522eacaaf43
P-ppc <ppc-user@foxmail.com>
2017-09-20 14:14:31 +0800

    移除调试信息

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ function createWindow () {
     }); 
 
     ipc.on("login-success", () => {
-        console.log("login success");
         win.setSize(600, 780, true);
         // win.loadURL(`file://${__dirname}/app/index.html`);
         // win.loadURL(`file://${__dirname}/app/login.html`);
